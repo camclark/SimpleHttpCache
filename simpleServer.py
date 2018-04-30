a6f905e827e16fe9f511d8f11cde9a6a983ae95d
Cameron Clark <cameron.clark1@my.jcu.edu.au>
2018-04-30 20:21:40 +1000

    add dynamic post json extraction

--- simpleServer.py
+++ simpleServer.py
@@ -3,6 +3,7 @@ import redis
 import re
 import cgi
 import json
+import urllib
 
 CACHE_SERVER_PORT_NUMBER = 8000
 CACHE_SERVER_ADDRESS = 'localhost'
@@ -15,15 +16,17 @@ r = redis.Redis(
     host=REDIS_SERVER_ADDRESS,
     port=REDIS_SERVER_PORT_NUMBER)
 
-# test set
-r.set(2019, {'id': 2019, 'message': 'Telstra 2019 Graduate Program'})
-r.expire('2019', 30)
+# # test set
+# r.set(2019, {'id': 2019, 'message': 'Telstra 2019 Graduate Program'})
+# r.expire('2019', 30)
+
+
 # print('set')
 
 class ServerCacheHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if None != re.search('/messages/*', self.path):
+        if None is not re.search('/messages/*', self.path):
             recordID = self.path.split('/')[-1]
             record = r.get(recordID)
 
@@ -48,6 +51,55 @@ class ServerCacheHandler(BaseHTTPRequestHandler):
             self.wfile.write("Forbidden".encode("utf-8"))
 
     def do_POST(self):
+        if None is not re.search('messages/*', self.path):
+            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+            if ctype == 'application/json':
+                length = int(self.headers.get_all('content-length')[0])
+                print(length)
+                # print(length)
+                post_data = self.rfile.read(length)
+                post_json_dict = json.loads(post_data)
+                print(type(post_json_dict))
+                print(post_json_dict["id"])
+                print(post_json_dict["message"])
+
+
+
+                # print("Type", type(self.rfile.read(length)))
+                # print(pdict)
+                # print(json.loads(pdict))
+
+
+                # data = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+
+                # # urllib.parse.parse_qs(self.rfile.read(length), keep_blank_values=True, strict_parsing=False,
+                #                       encoding='utf-8',
+                #                       errors='replace')
+                #
+                # # data = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+                # # recordID = self.path.split('/')[-1]
+                # # obj = json.loads(data)
+                # # LocalData.records[recordID] = data
+                #
+                #
+                # # data = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+                # # recordID = self.path.split('/')[-1]
+                # # LocalData.records[recordID] = data
+                # # print("record %s is added successfully" % recordID)
+
+
+                # # display back
+                # import json
+                # json_string = json.dumps(YOUR_DATA_STRUCTURE_TO_CONVERT_TO_JSON)
+                # self.wfile.write(json_string)
+
+            else:
+                self.send_response(200)
+                self.end_headers()
+        else:
+            self.send_response(403)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
         pass
 
 
